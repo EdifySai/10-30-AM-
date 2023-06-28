101c0803c17036ca4595394b66c66ebf499a3e3b
sai <sai@digital-lync.com>
2023-06-28 11:59:39 +0530

    28june project part-5

--- ReactJS/Project/frontendapp/src/User/Home/Home.js
+++ ReactJS/Project/frontendapp/src/User/Home/Home.js
@@ -1,9 +1,51 @@
 import { useState, useRef, useEffect } from "react";
 import { API } from '../../utils/constants';
 import APIHOC from '../../HOCs/APIHOC/APIHOC';
+import './Home.css';
 function Home(props) {
 
     const [products, setProducts] = useState([]);
+
+    const renderProductList = () => {
+
+        var list = products.map((product, i) => {
+            return <div key={product.productId} className="col-md-3 col-sm-6">
+                <div className="product-grid">
+                    <div className="product-image">
+                        <a href="#">
+                            <img className="pic-1" src={product.imageURL} style={{ width: '154px', height: '201px' }} />
+                            {/* <img className="pic-2" src="https://www.w3schools.com/bootstrap4/img_avatar3.png" /> */}
+                        </a>
+                        {/* <ul className="social">
+                           <li><a href="" data-tip="Quick View"><i className="fa fa-search"></i></a></li>
+                           <li><a href="" data-tip="Add to Wishlist"><i className="fa fa-shopping-bag"></i></a></li>
+                           <li><a href="" data-tip="Add to Cart"><i className="fa fa-shopping-cart"></i></a></li>
+                       </ul> */}
+                        {/* <span className="product-new-label">Sale</span>
+                       <span className="product-discount-label">20%</span> */}
+                    </div>
+                    <ul className="rating">
+                        <li className="fa fa-star"></li>
+                        <li className="fa fa-star"></li>
+                        <li className="fa fa-star"></li>
+                        <li className="fa fa-star"></li>
+                        <li className="fa fa-star disable"></li>
+                    </ul>
+                    <div className="product-content">
+                        <h3 className="title"><a href="#">{product.name}</a></h3>
+                        <div className="price">Rs.{product.price}
+                            {/* <span>$20.00</span> */}
+                        </div>
+                        {/* <a className="add-to-cart" href="">+ Add To Cart</a> */}
+                    </div>
+                </div>
+            </div>
+
+        })
+
+        return list;
+
+    }
 
     useEffect(() => {
         getProductsList();
@@ -47,6 +89,15 @@ function Home(props) {
                 <button className="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
                     <span className="carousel-control-next-icon"></span>
                 </button>
+            </div>
+            <br></br>
+            <hr></hr>
+
+            <div className="container">
+                <h3 className="h3">Top Products </h3>
+                <div className="row">
+                    {renderProductList()}
+                </div>
             </div>
         </div>
     )
